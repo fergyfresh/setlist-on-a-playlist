c9e6c4a0c4fec410734f5bb3944f72278e0bf7a0
William Ferguson <william.d.ferg@gmail.com>
2015-12-01 18:40:24 -0500

    Run the file to see some magic

--- doit.py
+++ doit.py
@@ -1,12 +1,31 @@
 import requests
 from BeautifulSoup import BeautifulSoup
 
-URL = 'http://www.setlist.fm/search?query='
-band_name = 'the+smith+street+band'
-r = request.get(URL+band_name)
+def make_band_showlist_url (band_name):
+    return 'http://www.setlist.fm/search?query=' \
+	   + "+".join(band_name.split())
 
-soup = BeatifulSoup(r.text)
+def find_newest_setlist_link (url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text)    
+    for a_tag in soup.findAll('a'):
+	link = a_tag.get('href')
+	try:
+    	    if a_tag.get('href').find('setlist/') == 0:
+        	setlist_link = link
+        	break	 
+	except:
+            pass
+    return 'http://www.setlist.fm/' + setlist_link
 
-for link in soup.findAll('a'):
-     print(link.get('href'))
+def get_setlist (band_name):
+    band_url = make_band_showlist_url(band_name)
+    newest_setlist = find_newest_setlist_link(band_url)
+    r = requests.get(newest_setlist)
+    soup = BeautifulSoup(r.text)
+    songlinks = soup.findAll("a", {"class":"songLabel"})
+    songlist = [song_link.string for song_link in songlinks]
+    return songlist
 
+test = get_setlist('the smith street band')
+    
